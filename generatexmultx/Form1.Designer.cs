8a21593da5aac627819d33cff14d08e7f02b6442
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-27 15:06:24 +0300

    Add project files.

---
+++ generatexmultx/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace generatexmultx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "x  X  x  =";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(251, 53);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(64, 22);
+            this.txtMin.TabIndex = 1;
+            this.txtMin.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Min";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(208, 81);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(386, 316);
+            this.txtOut.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(369, 52);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(64, 22);
+            this.txtMax.TabIndex = 4;
+            this.txtMax.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "generatePerfectSQRTIntegers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
